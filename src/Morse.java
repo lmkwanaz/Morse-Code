8c90f26a83ffd7ab6d92dce0d1ac5ff14c60a6a2
lmkwanaz <lmkwanaz>
2020-01-11 13:36:02 -0800

    done

--- src/Morse.java
+++ src/Morse.java
@@ -9,13 +9,13 @@ public class Morse {
                 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
                 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-                ',', '.', '?', '\f' };
+                ',', '.', '?', ' ' };
 
         String[] morse_code1 = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
                 ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
                 "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
                 "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-                "-----", "--..--", ".-.-.-", "..--..", "/"};
+                "-----", "--..--", ".-.-.-", "..--..", " "};
         String lower_str = code.toLowerCase();
         char[] changeit = lower_str.toCharArray();
         String newstr = "";
@@ -25,14 +25,17 @@ public class Morse {
                 for(int k=0; k<39; k++){
                     if(changeit[i] == morse_code[k]){
                         newstr += morse_code1[k] + " ";
-                    }
+                    }else if(Character.isWhitespace(changeit[i])){
+			newstr += "/ ";
+			break;
+		    }
                 }
             }
 
-            //System.out.println(newstr);
+            System.out.println(newstr);
 
-            assert newstr == lower_str : "this is wrong";
-            System.out.println("this is right");
+            //assert newstr == lower_str : "this is wrong";
+            ///System.out.println("this is right");
 
         }else{
             System.out.println("can you please put something");
@@ -76,13 +79,16 @@ public class Morse {
                     if(changeit[i].equals(morse_code1[k])){
 
                         newstr += morse_code[k];
-                    }
+                    }else if(changeit[i].equals("/")){
+			newstr += " ";
+			break;
+		    }
                 }
             }
             System.out.println(newstr);
             //System.out.println(newstr);
-            assert newstr == code : "this is wrong1";
-            System.out.println("this is right1");
+         //   assert newstr == code : "this is wrong1";
+           // System.out.println("this is right1");
         }else{
             System.out.println("can you please put something1");
         }
@@ -96,9 +102,7 @@ public class Morse {
         Scanner scan = new Scanner(System.in);
         System.out.println("please write something");
         String input = scan.nextLine();
-
-        //if(morseCodeToLetters(input)){}
-        lettersToMorseCode(input);
+        //lettersToMorseCode(input);
         morseCodeToLetters(input);
     }
 }
