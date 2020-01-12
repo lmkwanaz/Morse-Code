13f0afd3c6b8dde605b65f2ca481d1d67131cbb9
lmkwanaz <lmkwanaz>
2020-01-12 10:17:59 -0800

    done

--- src/Morse.java
+++ src/Morse.java
@@ -9,38 +9,39 @@ public class Morse {
                 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
                 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-                ',', '.', '?', ' ' };
+                ',', '.', '?' };
 
         String[] morse_code1 = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
                 ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
                 "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
                 "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-                "-----", "--..--", ".-.-.-", "..--..", " "};
+                "-----", "--..--", ".-.-.-", "..--.."};
         String lower_str = code.toLowerCase();
         char[] changeit = lower_str.toCharArray();
         String newstr = "";
+	int count = 0;
 
         if(code.length() != 0){
             for(int i=0; i<changeit.length;i++){
                 for(int k=0; k<39; k++){
                     if(changeit[i] == morse_code[k]){
                         newstr += morse_code1[k] + " ";
+			count++;
                     }else if(Character.isWhitespace(changeit[i])){
 			newstr += "/ ";
+			count++; 
 			break;
 		    }
                 }
             }
 
+	     assert count == code.length() : "this is totaly wrong in morseToCode";
             System.out.println(newstr);
 
-            //assert newstr == lower_str : "this is wrong";
-            ///System.out.println("this is right");
-
         }else{
-            System.out.println("can you please put something");
+            System.out.println("this field shouldn't be empty");
         }
-        return newstr;
+        return code;
     }
 
 
@@ -72,25 +73,27 @@ public class Morse {
         String[] changeit = code.split(" ");
 
         String  newstr = " ";
+	int morse_code_count = 0;
 
         if(code.length() != 0){
             for(int i=0; i<changeit.length;i++){
                 for(int k=0; k<39 ;k++){
                     if(changeit[i].equals(morse_code1[k])){
-
+			morse_code_count++;
                         newstr += morse_code[k];
                     }else if(changeit[i].equals("/")){
 			newstr += " ";
+			morse_code_count++;
 			break;
 		    }
                 }
             }
+            
+             assert morse_code_count == newstr.length() - 1 : "this is totaly wrong in morseToCode";
             System.out.println(newstr);
-            //System.out.println(newstr);
-         //   assert newstr == code : "this is wrong1";
-           // System.out.println("this is right1");
+
         }else{
-            System.out.println("can you please put something1");
+            System.out.println("this field shouldn't  be empty");
         }
 
         return newstr;
